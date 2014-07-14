282148cdf8e5ea1b9ddb8dabba787f86e8802a1c
Robpol86 <robpol86@gmail.com>
2014-07-13 20:51:29 -0700

    Adding to Travis CI

--- setup.py
+++ setup.py
@@ -1,22 +1,40 @@
 import ast
-import os
 from codecs import open  # To use a consistent encoding
+import os
 import setuptools
 import setuptools.command.sdist
+from setuptools.command.test import test
+import sys
 
 setuptools.command.sdist.READMES = tuple(list(setuptools.command.sdist.READMES) + ['README.md'])
 here = os.path.abspath(os.path.dirname(__file__))
 
+
 # Get the long description and other data from the relevant files
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
-with open(os.path.join(here, 'flask_redis.py')) as f:
+with open(os.path.join(here, 'flask_redis.py'), encoding='utf-8') as f:
     lines = [l.strip() for l in f if l.startswith('__')]
 metadata = ast.literal_eval("{'" + ", '".join([l.replace(' = ', "': ") for l in lines]) + '}')
 __author__, __license__, __version__ = [metadata[k] for k in ('__author__', '__license__', '__version__')]
 if not all((__author__, __license__, __version__)):
     raise ValueError('Failed to obtain metadata from module.')
 
+
+class PyTest(test):
+    def finalize_options(self):
+        test.finalize_options(self)
+        setattr(self, 'test_args', ['tests'])
+        setattr(self, 'test_suite', True)
+
+    def run_tests(self):
+        # Import here, cause outside the eggs aren't loaded.
+        pytest = __import__('pytest')
+        err_no = pytest.main(self.test_args)
+        sys.exit(err_no)
+
+
+# Setup definition.
 setuptools.setup(
     name='Flask-Redis-Helper',
     version=__version__,
@@ -68,4 +86,7 @@ setuptools.setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
     install_requires=['Flask', 'redis'],
+
+    tests_require=['pytest'],
+    cmdclass=dict(test=PyTest),
 )
